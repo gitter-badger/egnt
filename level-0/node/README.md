e2eb2343e59ab59fbe6f3e079570f2ce9bfeb154
janroesler <janroesler@googlemail.com>
2014-09-26 02:14:29 +0200

    Der Server antwortet auf "hallo"
    Erweiterung um socket.io und angularjs.
    + Klicki Bunti

--- level-0/node/README.md
+++ level-0/node/README.md
@@ -4,7 +4,15 @@
 
 * Level 0 - Step 1:
  - [express](https://www.npmjs.org/package/express)
- - [socket.io](http://socket.io/get-started/chat/)
 
 [Hier](https://github.com/burning-duck/egnt/wiki/node.js---level-0)
 geht es zum Hallo Welt! mit express.
+
+---
+
+* Level 0 - Step 2:
+ - [express](https://www.npmjs.org/package/express)
+ - [socket.io](http://socket.io/get-started/chat/)
+ - [angular](https://angularjs.org/)
+
+ Klicki Bunti + der Server antwortet auf "hallo".
--- level-0/node/server.js
+++ level-0/node/server.js
@@ -1,14 +1,34 @@
-
+// require
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// static files
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules'));
+app.use(express.static(__dirname + '/bower_components'));
 
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-
 http.listen(3000, function(){
   console.log('listening on *:3000');
   console.log(__dirname);
+});
+
+
+// connection success
+io.sockets.on('connection', function(socket) {
+    console.log('client connected!');
+    
+    socket.on('send', function(message) {
+    	console.log("Server: " + message);
+
+    	if (message === "hallo") {
+    		socket.emit('back', "Selber Hallo!");
+    	};
+    });
 });
